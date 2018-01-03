c51468a7707d272c6402abf0de9891c76ff6cc5f
David Meyer <david.t.meyer@gmail.com>
2018-01-03 11:08:31 -0500

    added session to simulate different devices

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 let express = require('express'),
+session = require('express-session'),
+cookieParser = require('cookie-parser'),
     app = express(),
     port = process.env.PORT || 5000,
     http = require("http"),
@@ -7,37 +9,46 @@ let express = require('express'),
     jsforce = require('jsforce'),
     conn = new jsforce.Connection();
 
-//login to sfdc/create connection
-conn.login(username, password, function(err, res) {
-    if (err) { return console.error(err); }
-});
-
-
+app.use(cookieParser());
+app.use(session({
+    secret: process.env.SECRET,
+    resave: false,
+    saveUninitialized: true
+}));
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 app.disable('view cache');
 app.use(express.static(__dirname + '/public'));
 
+//login to sfdc/create connection
+conn.login(username, password, function(err, res) {
+    if (err) { return console.error(err); }
+});
+
+//handle get to /
 app.get('/',
     function(req, res) {
+        
         let door = (req.param('door')) ? req.param('door') : 'closed';
-        moveDoor(door);
+        let sid = req.sessionID;
+        let deviceId = 'node:'+sid.substring(0,11);//simulate deviceIds
+        moveDoor(door,deviceId);
         res.render('index', {
             session: req.session,
         });
     });
 
-let moveDoor = function(door) {
+let moveDoor = function(door,deviceId) {
     let ts = new Date;
     conn.sobject("Smart_Fridge_Reading__e").create({
-        deviceId__c: 'node1',
+        deviceId__c: deviceId,
         door__c: door, // closed, open
         humidity__c: 40.0,
         temperature__c: 5.0,
         ts__c: ts
     }, function(err, ret) {
         if (err || !ret.success) { return console.error(err, ret); }
-        console.log("moved the door to " + door +' :' + ret.id);
+        console.log("moved the door to " + door +' for deviceId=> '+deviceId+' :' + ret.id);
     });
 };
 
