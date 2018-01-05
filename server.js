5685ba7fc21b1a3b638c270cbd71f4edd9bb62db
David Meyer <david.t.meyer@gmail.com>
2018-01-05 17:48:35 -0600

    humidity & temp buttons

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ let express = require('express'),
     PASSWORD = process.env.PASSWORD,
     DOOR_OPEN = 'open',
     DOOR_CLOSED = 'closed',
-    ABOVE_SAFE_HUMIDITY = 'above-safe-humididity',
-    ABOVE_SAFE_TEMPERATURE = 'above-safe-temperature',
+    ABOVE_SAFE_HUMIDITY = 'humidity',
+    ABOVE_SAFE_TEMPERATURE = 'temperature',
     jsforce = require('jsforce'),
     conn = new jsforce.Connection(),
     sleep = require('sleep');
@@ -34,58 +34,37 @@ conn.login(USERNAME, PASSWORD, function(err, res) {
 app.get('/',
     function(req, res) {
 
-        let door = (req.param('door')) ? req.param('door') : 'closed';
-        if (door === 'multi') {
-            multipleFridges(0, 9, 0); //counter, #of events to fire, getEventTypes
+        let state = (req.param('state')) ? req.param('state') : 'closed';
+        if (state === 'multi') {
+            //multipleFridges1k(0, 9, 0); //counter, #of events to fire, getEventTypes
         } else {
             let sid = req.sessionID;
             let deviceId = 'node:' + sid.substring(0, 11); //simulate deviceIds
-            moveDoor(door, deviceId);
+            setState(state, deviceId);
         }
         res.render('index', {
             session: req.session,
         });
     });
 
-let moveDoor = function(door, deviceId) {
-    let ts = new Date;
-    conn.sobject("Smart_Fridge_Reading__e").create({
-        deviceId__c: deviceId,
-        door__c: door, // closed, open
-        humidity__c: 40.0,
-        temperature__c: 5.0,
-        ts__c: ts
-    }, function(err, ret) {
+let setState = function(door, deviceId) {
+    let resetEvent = getEvent(DOOR_CLOSED, deviceId);
+    console.log(resetEvent)
+    conn.sobject("Smart_Fridge_Reading__e").create(resetEvent, function(err, ret) {
         if (err || !ret.success) { return console.error(err, ret); }
-        console.log("moved the door to " + door + ' for deviceId=> ' + deviceId + ' :' + ret.id);
+        console.log('reset to closed for deviceId=> ' + deviceId + ' :' + ret.id);
+//        sleep.sleep(1);
+        let event = getEvent(door, deviceId);
+        console.log(event)
+        conn.sobject("Smart_Fridge_Reading__e").create(event, function(err, ret) {
+            if (err || !ret.success) { return console.error(err, ret); }
+            console.log("state set to " + door + ' for deviceId=> ' + deviceId + ' :' + ret.id);
+        });
     });
-};
-
-let multipleFridges = function(i, NUMBER_OF_EVENTS, which) {
-    /* 
-     * making individual calls because if a batch them 
-     * the ui changes in bulk and is not as visually appealing
-     */
-
-    let state = getState(which, i);
-//    let e = getEvent(state, 'node:' + which +':'+ i);
-    let e = getEvent(state, 'node:' + i);
-    conn.sobject("Smart_Fridge_Reading__e").create(e, function(err, ret) {
-        if (err || !ret.success) { return console.error(err, ret); }
-        console.log(ret.id)
-        console.log(which)
-        if (i < NUMBER_OF_EVENTS) {
-            sleep.msleep(200);
-            multipleFridges(++i, NUMBER_OF_EVENTS, which);
-        } else if (which < 2) {//3 groups of events, 0,1,2
-            sleep.sleep(7);
-            multipleFridges(0, NUMBER_OF_EVENTS, ++which);
-        }
-    });
-
 };
 
 let getEvent = function(state, deviceId) {
+
     let ts = new Date;
     let event = {
         deviceId__c: deviceId,
@@ -94,10 +73,61 @@ let getEvent = function(state, deviceId) {
         temperature__c: (state == ABOVE_SAFE_TEMPERATURE) ? 55.0 : 5.0,
         ts__c: ts
     }
-    console.log(state)
-    console.log(event)
+    if (state == ABOVE_SAFE_HUMIDITY) {
+        delete event.door__c;
+        delete event.temperature__c;
+    } else if (state == ABOVE_SAFE_TEMPERATURE) {
+        delete event.door__c;
+        delete event.humidity__c;
+    }
     return event;
 }
+
+let multipleFridges = function() {
+
+}
+
+
+
+app.listen(port, function() {
+    console.log('Express server listening on port ' + port);
+});
+
+
+
+
+
+/**
+ ** not used, might use in the future for enterprise org w/ more devices & events
+ **
+ **
+ **
+ */
+
+let multipleFridges1k = function(i, NUMBER_OF_EVENTS, which) {
+    /* 
+     * making individual calls because if a batch them 
+     * the ui changes in bulk and is not as visually appealing
+     */
+
+    let state = getState(which, i);
+    //    let e = getEvent(state, 'node:' + which +':'+ i);
+    let e = getEvent(state, 'node:' + i);
+    conn.sobject("Smart_Fridge_Reading__e").create(e, function(err, ret) {
+        if (err || !ret.success) { return console.error(err, ret); }
+        console.log(ret.id)
+        console.log(which)
+        if (i < NUMBER_OF_EVENTS) {
+            multipleFridges(++i, NUMBER_OF_EVENTS, which);
+        } else if (which < 2) { //3 groups of events, 0,1,2
+            sleep.sleep(7);
+            multipleFridges(0, NUMBER_OF_EVENTS, ++which);
+        }
+    });
+
+};
+
+
 
 let getState = function(which, i) {
     let state = DOOR_CLOSED;
@@ -114,8 +144,3 @@ let getState = function(which, i) {
     // 2 - DOOR_CLOSED
     return state;
 }
-
-
-app.listen(port, function() {
-    console.log('Express server listening on port ' + port);
-});
